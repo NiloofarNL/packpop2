c0d5f8c49ec6687b8f4209b8c86dab48ce1bd13f
NiloofarNL <niloofar.nalaee@gmail.com>
2021-12-16 07:52:16 -0800

    Update

--- R/packpop2-package.R
+++ R/packpop2-package.R
@@ -19,21 +19,24 @@ NULL
 
 #' Population of Mashhad in 2016.
 #'
-#' A dataset containing information about population based on number of families, owners, tenants and residential units of all blocks in Mashhad, Iran.
+#' A dataset containing information about population based on number of families, owners, tenants and residential units of all neighbourhoods in Mashhad, Iran.
 #'
 #' @format A data frame with 19943 rows and 11 variables:
 #' \describe{
-#'   \item{OBJECTID}{Id of each block as a factor}
-#'   \item{population}{Number of whole people living in a block}
-#'   \item{Family}{Number of families living in a block}
-#'   \item{Owners}{Number of people who has their own property in a block}
-#'   \item{Tenants}{Number of people who rent a property in a block}
-#'   \item{Residential Units}{Whole number of residencial suits in a block}
-#'   \item{Apartments}{Number of suits in apartments in a block}
-#'   \item{Houses}{Number of village houses in a block}
-#'   \item{Cottage or shed}{Number of sheds in a block}
-#'   \item{Shape_Length}{Surrounding a block in meter scale}
-#'   \item{Shape_Area}{Surface area of a block in meter scale}
+#'   \item{OBJECTID}{Id of each neighbourhoods as a factor}
+#'   \item{population}{Number of whole people living in a neighbourhoods}
+#'   \item{Family}{Number of families living in a neighbourhoods}
+#'   \item{Owners}{Number of people who has their own property in a neighbourhoods}
+#'   \item{Tenants}{Number of people who rent a property in a neighbourhoods}
+#'   \item{Residential Units}{Whole number of residencial suits in a neighbourhoods}
+#'   \item{Apartments Units}{Number of suits in apartments in a neighbourhoods}
+#'   \item{Houses Units}{Number of village houses in a neighbourhoods}
+#'   \item{Cottages or sheds}{Number of sheds in a neighbourhoods}
+#'   \item{Shape_Length}{Surrounding a neighbourhoods in meter scale}
+#'   \item{Shape_Area}{Surface area of a neighbourhoods in meter scale}
+#'   \item{Area(Hectare)}{Surface area of a neighbourhoods in hectare scale}
+#'   \item{Populatoin Density}{Population density of each neighbourhood}
+#'   \item{Populatoin}{Whole population of each neighbourhood}
 #' }
 #'
 #' @docType data
